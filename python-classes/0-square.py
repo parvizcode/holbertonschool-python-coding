37229d5fc8ca8df698b9f9cbca612c4e30316a3e
parvizcode <gparvizn@gmail.com>
2025-06-26 06:19:48 +0000

    Add Square class with private size attribute

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Tanımlanmış bir kare sınıfı."""
+
+
+class Square:
+    """Bir kare sınıfı tanımı."""
+
+    def __init__(self, size):
+        """Kareyi verilen boyutla başlatır."""
+        self.__size = size
